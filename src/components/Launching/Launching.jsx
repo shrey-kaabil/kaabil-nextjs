5d7c347e54b6ff8af134a618709ba238299fe7ac
Ashwin P <apiprotar92@gmail.com>
2025-04-30 02:15:20 +0530

    Pricing card section design web and mobile view design

--- src/components/Launching/Launching.jsx
+++ src/components/Launching/Launching.jsx
@@ -1,76 +1,142 @@
-import React from 'react';
-import './Launching.css';
-import  ThumbsUp  from '../../assets/thums-up.svg';
-import  ThumbsDown  from '../../assets/thums-down.svg';
-import Image from 'next/image';
+import React from "react";
+import "./Launching.css";
+import ThumbsUp from "../../assets/thums-up.svg";
+import ThumbsDown from "../../assets/thums-down.svg";
+import PricingShap1 from "../../assets/pricing-shap1.svg";
+import Image from "next/image";
 
 const Launching = () => {
   return (
     <section id="launching" className="launching-program-beta">
-      <div className="section-title">
-        <h2>Launching 1-Month FREE<br />Beta Program</h2>
-      </div>
-
-      <div className="launching-program">
-        <div className="alternative-platforms">
-          <h2>Current Resources</h2>
-          <h1>₹ 600-2000</h1>
-          <ul className="feature-list">
-            <li>
-              <Image src={ThumbsDown} alt="thumsUp" width={24} height={24} />
-              Limited Pre-design Questions
-            </li>
-            <li>
-              <Image src={ThumbsDown} alt="thumsDown" width={24} height={24} />
-              Doubt Video Solutions
-            </li>
-            <li>
-              <Image src={ThumbsDown} alt="thumsDown" width={24} height={24} />
-              A Few Days Delay in Feedback
-            </li>
-            <li>
-              <Image src={ThumbsDown} alt="thumsDown" width={24} height={24} />
-              Relying On Scattered PDFs
-            </li>
-            <li>
-              <Image src={ThumbsDown} alt="thumsDown" width={24} height={24} />
-              Surface Level Analytics
-            </li>
-          </ul>
+      <div className="container">
+        <div className="section-title">
+          <h2>Launching 1-Month FREE Beta Program</h2>
         </div>
 
-        <div className="kaabil-card">
-          <h2>Kaabil Learning</h2>
-          <h1>FREEE !!</h1>
-          <ul className="feature-list">
-            <li>
-              <Image src={ThumbsUp} alt="thumsUp" width={24} height={24} />
-              Unlimited Customisable Questions
-            </li>
-            <li>
-              <Image src={ThumbsUp} alt="thumsUp" width={24} height={24} />
-              AI-Powered Kaabil TAs Available 24/7
-            </li>
-            <li>
-              <Image src={ThumbsUp} alt="thumsUp" width={24} height={24} />
-              Instant Answer Sheet Feedback
-            </li>
-            <li>
-              <Image src={ThumbsUp} alt="thumsUp" width={24} height={24} />
-              Make Tests from Your Own PDFs
-            </li>
-            <li>
-              <Image src={ThumbsUp} alt="thumsUp" width={24} height={24} />
-              Personalized Revision & Recommendations
-            </li>
-          </ul>
-          <button className="start-learning-btn">
-            Start Learning
-          </button>
+        <div className="launching-program">
+          <div className="alternative-platforms">
+            <h2>Alternative Platforms</h2>
+            <h1>₹ 600-2000</h1>
+            <ul className="feature-list">
+              <li>
+                <Image
+                  src={ThumbsDown}
+                  alt="thums Down"
+                  width={40}
+                  height={40}
+                  className="thums-down-icon"
+                />
+                Pre-Designed Question Banks
+              </li>
+              <li>
+                <Image
+                  src={ThumbsDown}
+                  alt="thumsDown"
+                  width={40}
+                  height={40}
+                  className="thums-down-icon"
+                />
+                Limited Doubt Support
+              </li>
+              <li>
+                <Image
+                  src={ThumbsDown}
+                  alt="thumsDown"
+                  width={40}
+                  height={40}
+                  className="thums-down-icon"
+                />
+                Hours to Days for feedback
+              </li>
+              <li>
+                <Image
+                  src={ThumbsDown}
+                  alt="thumsDown"
+                  width={40}
+                  height={40}
+                  className="thums-down-icon"
+                />
+                Reliance on Scattered Xerox
+              </li>
+              <li>
+                <Image
+                  src={ThumbsDown}
+                  alt="thumsDown"
+                  width={40}
+                  height={40}
+                  className="thums-down-icon"
+                />
+                Surface Level Insights only
+              </li>
+            </ul>
+          </div>
+
+          <div className="kaabil-card">
+            <h2>Kaabil Learning</h2>
+            <h1>FREEE !!</h1>
+            <ul className="feature-list">
+              <li>
+                <Image
+                  src={ThumbsUp}
+                  alt="thumsUp"
+                  width={40}
+                  height={40}
+                  className="thums-up-icon"
+                />
+                Unlimited Customisable Questions
+              </li>
+              <li>
+                <Image
+                  src={ThumbsUp}
+                  alt="thumsUp"
+                  width={40}
+                  height={40}
+                  className="thums-up-icon"
+                />
+                24/7 Available AI-Powered Kaabil TAs
+              </li>
+              <li>
+                <Image
+                  src={ThumbsUp}
+                  alt="thumsUp"
+                  width={40}
+                  height={40}
+                  className="thums-up-icon"
+                />
+                Instant Answer Sheet Feedback
+              </li>
+              <li>
+                <Image
+                  src={ThumbsUp}
+                  alt="thumsUp"
+                  width={40}
+                  height={40}
+                  className="thums-up-icon"
+                />
+                Make Tests from your own PDFs
+              </li>
+              <li>
+                <Image
+                  src={ThumbsUp}
+                  alt="thumsUp"
+                  width={40}
+                  height={40}
+                  className="thums-up-icon"
+                />
+                Personalized Revision & Recommendations
+              </li>
+            </ul>
+            <div className="text-center">
+              <button className="start-learning-btn">Start Learning</button>
+            </div>
+          </div>
+        </div>
+        <div className="pricing-vector-shap1 position-absolute">
+          <Image src={PricingShap1} alt="shap" width={559} height={515} />
         </div>
       </div>
     </section>
   );
 };
 
-export default Launching; 
+export default Launching;
